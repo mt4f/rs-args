10b7be2a4b16d88049cf18ebc237747b74b8385b
Carl Neben <carl-neben@gmx.de>
2023-12-28 23:28:43 +0100

    Version 0.1.2 - Lexer Fixed - Fixed a bug in the lexer where the string wouldn't be recognized - Added Readme file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use rs_args::lexing;
 
 fn main() {
-    let input = "1223.4556 asdf";
-    let mut tokens = lexing::tokenise(String::from(input));
+    let input = "1223.4556 asdf \"Hello, World!\"";
+    let tokens = lexing::tokenise(String::from(input));
 
     for token in tokens {
         println!("{:?}", token);
